c7a8f4676478fd660a26f0ff264b6615dc6d934a
Harshal-Jadhav <103627954+Harshal-Jadhav@users.noreply.github.com>
2022-07-06 03:22:41 +0530

    Assignment completed

---
+++ scripts/video.js
@@ -0,0 +1,9 @@
+let data = JSON.parse(localStorage.getItem('video'))
+console.log(data);
+
+
+let append = (data) => {
+    document.querySelector('iframe').src = 'https://www.youtube.com/embed/'+data.id
+    document.querySelector('#vidName').innerText = data.snippet.title
+}
+append(data)
